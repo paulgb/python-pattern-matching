0955e08bcecb9fe601e01526e9d8de62068f437c
Paul <paul@wildcard.(none)>
2009-02-20 22:18:38 -0800

    Code clean-up of patmatch.py and tests

--- patmatch.py
+++ patmatch.py
@@ -15,30 +15,37 @@ TODO:
  - readme file
  - license file
  - make cons reasonably fast
- - better inline docs
- - have a pattern that matches anything?
  - put the classes in a logical order, possibly in separate files
+ - make into a python module
 """
 
-class MatchExpression():
+class MatchPattern():
     """
-    Dummy class which match expressions can extend.
+    Dummy class which match patterns can extend.
 
-    Subclasses of MatchExpression must implement one method:
-    match(self, value, match_values_box)
+    Subclasses of MatchPattern must implement one method:
+    match(self, value, match_values_box, cap_val)
 
-    value is the value to be matched, match_values_box is a
-    box to return a CapturedValues object to the original call
-    to match.
+    value is the value to be matched
+    match_values_box is a box to return a CapturedValues object
+      to the original call to match.
+    cap_val is a dict for the values captured so far in the match.
     """
     pass
 
 
-class MatchEq(MatchExpression):
+
+class MatchEq(MatchPattern):
     """
-    Explicit equality match.
+    Explicit equality match. Alias is `eq`.
 
     Matches only if subject is equal to the given value.
+
+    Examples:
+    >>> match(eq(5), 5)
+    True
+    >>> match(eq(int), 5)
+    False
     """
     
     def __init__(self, value):
@@ -48,13 +55,45 @@ class MatchEq(MatchExpression):
     def match(self, value, match_values_box, cap_val):
         return value == self.value
 
+
 eq = MatchEq
 
 
-class MatchOb(MatchExpression):
+
+class MatchOb(MatchPattern):
+    """
+    Match an object, optionally matching attributes of that
+    object. Alias is `ob`.
+
+    Constructor takes one optional paramater (the class of the object to be
+    matched), followed by as many named paramaters as are neccesary.
+
+    The values of the named paramaters should be patterns accepted by match().
+    The attributes of the object will be checked against the patterns given by
+    the same name.
+
+    Examples:
+    >>> class Animal:
+    ...     legs = None
+    ...     habitat = None
+    >>> fish = Animal
+    >>> fish.habitat = 'water'
+    >>> fish.legs = 0
+    >>> match(ob(Animal), fish)
+    True
+    >>> match(ob(Animal, legs=0), fish)
+    True
+    >>> match(ob(legs=0, habitat='water'), fish)
+    True
+    >>> match(ob(Animal, legs=4), fish)
+    False
+    """
+
     def __init__(self, *args, **patterns):
         self.cls = None
         if args:
+            if len(args) != 1:
+                raise TypeError, "ob() takes at most 1 non-named arguments."
             self.cls = args[0]
         self.patterns = patterns
         
@@ -63,14 +102,28 @@ class MatchOb(MatchExpression):
         for attr, pattern in self.patterns.iteritems():
             if not hasattr(value, attr):
                 return False
-            if not match_recur(pattern, getattr(value, attr), match_values_box, cap_val):
+            if not match_recur(pattern, getattr(value, attr),
+              match_values_box, cap_val):
                 return False
         return True
     
+
 ob = MatchOb
 
 
-class MatchDict(MatchExpression):
+
+class MatchDict(MatchPattern):
+    """
+    Match for a dictionary, optionally matching against certain
+    values of the dictionary. Alias is `dt()`.
+
+    Examples:
+    >>> match(dt(a=4, b='foo'), {'a': 4, 'b': 'foo', 'j': 9})
+    True
+    >>> match(dt(a=4, b='foo'), {'a': 4, 'j': 9})
+    False
+    """
+
     def __init__(self, **patterns):
         self.patterns = patterns
 
@@ -83,28 +136,38 @@ class MatchDict(MatchExpression):
                 return False
         return True
 
+
 dt = MatchDict
 
 
-class MatchType(MatchExpression):
+
+class MatchType(MatchPattern):
     """
-    Explicit type match.
+    Explicit type match. Alias is `type_is().`
 
     Matches only if the subject is an instance of the given type.
+
+    >>> match(type_is(int), 3)
+    True
+    >>> match(type_is(int), int)
+    False
     """
 
     def __init__(self, type):
         self.type = type
 
+
     def match(self, value, match_values_box, cap_val):
         return isinstance(value, self.type)
+
 
 type_is = MatchType
 
 
-class MatchPredicate(MatchExpression):
+
+class MatchPredicate(MatchPattern):
     """
-    Match based on a predicate function. Alias is pred().
+    Match based on a predicate function. Alias is `pred()`.
     
     A value matches if and only if fun(value) is True.
 
@@ -115,19 +178,23 @@ class MatchPredicate(MatchExpression):
     False
 
     """
+
     def __init__(self, fun):
         self.fun = fun
+
 
     def match(self, value, match_values_box, cap_val):
         return self.fun(value)
 
+
 pred = MatchPredicate
 
 
-class MatchCons(MatchExpression):
+
+class MatchCons(MatchPattern):
     """
-    Match a python list as if it were a cons cell. Alias is
-    cons.
+    Match a python list as if it were a lisp cons cell. Alias is
+    `cons()`.
 
     A cons cell is a basic implementation of a linked list,
     where each element is stored as a (car, cdr) pair.
@@ -140,7 +207,7 @@ class MatchCons(MatchExpression):
 
     Note that the current implementation is not efficient,
     the list is copied many times. This should not yet be
-    used on large lists.
+    used on large lists. @@
     
     Examples:
     
@@ -154,48 +221,37 @@ class MatchCons(MatchExpression):
         self.car_pattern = car_pattern
         self.cdr_pattern = cdr_pattern
     
+
     def match(self, value, match_values_box, cap_val):
         if type(value) is not list:
             return False
         
         if match_recur(self.car_pattern, value[0], match_values_box, cap_val):
-            return match_recur(self.cdr_pattern, value[1:], match_values_box, cap_val)
+            return match_recur(self.cdr_pattern, value[1:],
+              match_values_box, cap_val)
 
         return False
+
 
 cons = MatchCons
 
 
-class ValueBox(MatchExpression):
+
+class ValueBox(MatchPattern):
     """
     A match expression that will match any expression and store
-    its value in a box. The box can be accessed later. This is
-    the mechanism used for value capture.
-
-    Examples:
-
-    The ValueBox can be used directly:
-    >>> a = ValueBox()
-    >>> match(a, 8)
-    True
-    >>> a.get()
-    8
-
-    Or via an instance of CapturedValues:
-    >>> mv = CapturedValues()
-    >>> match(mv.a, 8) # mv.a is a ValueBox instance
-    True
-    >>> mv.a
-    8
-    
+    its value in cap_val. This is the mechanism used for value
+    capture.
     """
 
     def __init__(self, name, match_values=None):
         self.name = name
         self.match_values = match_values
 
+
     def get(self):
         return self.value
+
 
     def match(self, value, match_values_box, cap_val):
         if len(match_values_box) != 0:
@@ -209,14 +265,16 @@ class ValueBox(MatchExpression):
             cap_val[self.name] = value
             return True
 
+
     def __repr__(self):
         return "<ValueBox: %s>" % repr(self.value)
 
 
-class MatchAll(MatchExpression):
+
+class MatchAll(MatchPattern):
     """
     Match a value only if all of the given paterns
-    are satisfied by the value. Alias is all().
+    are satisfied by the value. Alias is `all()`.
 
     Examples:
     >>> match(all(int, pred(lambda x: x < 9)), 8)
@@ -234,11 +292,11 @@ class MatchAll(MatchExpression):
     4
     >>> mv.b
     'bingo'
-    
     """
 
     def __init__(self, *patterns):
         self.patterns = patterns
+
 
     def match(self, value, match_values_box, cap_val):
         for pattern in self.patterns:
@@ -246,13 +304,15 @@ class MatchAll(MatchExpression):
                 return False
         return True
 
+
 all = MatchAll
 
 
-class MatchSome(MatchExpression):
+
+class MatchSome(MatchPattern):
     """
     Match a value if at least one of the given patterns
-    is matched by the value.
+    is matched by the value. Alias is `some()`.
 
     Examples:
     >>> match(some(int, str), 4)
@@ -267,6 +327,7 @@ class MatchSome(MatchExpression):
     def __init__(self, *patterns):
         self.patterns = patterns
     
+
     def match(self, value, match_values_box, cap_val):
         for pattern in self.patterns:
             new_cap_val = dict(cap_val)
@@ -275,7 +336,10 @@ class MatchSome(MatchExpression):
                 return True
         return False
 
+
 some = MatchSome
+
+
 
 class CapturedValues():
     """
@@ -283,18 +347,19 @@ class CapturedValues():
     values.
 
     After a match is complete, the CapturedValues instance
-    is switched into read mode, where it returns the values
-    of the ValueBoxes instead of the boxes themselves.
+    is switched into read mode, where it returns the matched
+    values.
 
     >>> cv = CapturedValues()
     >>> match(cv.a, 8)
     True
     >>> cv.a
     8
-    
     """
+
     def __init__(self):
         self._clear()
+
 
     def __getattr__(self, name):
         if self._read_mode:
@@ -302,20 +367,26 @@ class CapturedValues():
         else:
             return ValueBox(name, self)
 
+
     def _close(self, values):
         self._values = values
         self._read_mode = True
+
 
     def _destroy(self):
         self._values = {}
         self._read_mode = True
 
+
     def _clear(self):
         self._values = None
         self._read_mode = False
 
+
     def __repr__(self):
         return "<CapturedValues>"
+
+
 
 def match(pattern, subject):
     """
@@ -329,8 +400,8 @@ def match(pattern, subject):
     True
     >>> match([1, int, (str, bool)], [1, "bar", ("foo", False)])
     False
-
     """
+
     match_values_box = []
     cap_val = {}
     matched = match_recur(pattern, subject, match_values_box, cap_val)
@@ -342,13 +413,14 @@ def match(pattern, subject):
     return matched
 
 
+
 def match_recur(pattern, subject, match_values_box, cap_val):
     """
     Recursive match function. Shouldn't be called directly
     except from match.
     """
 
-    if isinstance(pattern, MatchExpression):
+    if isinstance(pattern, MatchPattern):
         return pattern.match(subject, match_values_box, cap_val)
     
     if pattern == subject:
--- patmatch_test.py
+++ patmatch_test.py
@@ -1,38 +1,46 @@
 
 import unittest
 
-from patmatch import match, CapturedValues, ValueBox, all, some, pred, cons, eq, type_is, ob, dt
+from patmatch import match, CapturedValues, ValueBox, all, \
+    some, pred, cons, eq, type_is, ob, dt
 
 class TestPatmatch(unittest.TestCase):
     def testValue(self):
+        # Test that equal values match.
         assert match(1, 1)
         assert not match(1, 'foo')
         assert not match(1, 2)
 
 
     def testSequence(self):
+        # Test that a tuple is matched
         assert match((1, int), (1, 2))
         assert not match((1, 2), [1, 2])
 
 
     def testDeepSequence(self):
+        # Test that a list is matched recursively
         assert match([(int, (str, dict))], [(3, ("foo", {}))])
 
 
     def testTypesMatch(self):
+        # Test that when match is given a type as a pattern, it matches any
+        # value of that type
         assert match([int, str, dict], [2, 'foo', {}])
-        assert not match([int, str], [2, 'foo', {}])
-        assert not match([int, int, int], [2, 'foo', {}])
+        assert not match([int, str], [2, 'foo', {}]) # length matters
+        assert not match([int, int, int], [2, 'foo', {}]) # type matters
         assert match(dict, {})
 
 
     def testCaptureValues(self):
+        # Test capturing values from the expression
         cv = CapturedValues()
         assert match(cv.i, 3)
         self.assertEquals(3, cv.i)
 
 
     def testCaptureBadMatch(self):
+        # Test behaviour when pattern is not matched
         cv = CapturedValues()
         assert not match([(cv.a, cv.b), int], [(5, "bar"), "foo"])
         try:
@@ -43,6 +51,7 @@ class TestPatmatch(unittest.TestCase):
 
 
     def testDeepCapture(self):
+        # Test capture recursively
         cv = CapturedValues()
         assert match([[[cv.a, cv.b], (cv.c, cv.d)], 5], [[[1, 2], (3, 4)], 5])
         self.assertEquals(1, cv.a)
@@ -52,13 +61,16 @@ class TestPatmatch(unittest.TestCase):
 
     
     def testAll(self):
+        # Test all() match pattern
         cv = CapturedValues()
         assert match(all(cv.a, int), 4)
         self.assertEquals(4, cv.a)
         assert not match(all(cv.a, int), 9)
         assert match(all(), 4)
     
+
     def testSome(self):
+        # Test some() match pattern
         cv = CapturedValues()
         assert match(some(int, str), "abc")
         assert not match(some(int, str), [])
@@ -66,11 +78,13 @@ class TestPatmatch(unittest.TestCase):
         self.assertEquals(1, cv.a)
         
         cv = CapturedValues()
-        assert match(some((cv.a, (cv.b, (cv.c, ()))), [cv.a, cv.b, cv.c]), (1, (2, (3, ()))))
+        assert match(some((cv.a, (cv.b, (cv.c, ()))), [cv.a, cv.b, cv.c]),
+          (1, (2, (3, ()))))
         self.assertEquals(1, cv.a)
 
         cv = CapturedValues()
-        assert match(some((cv.a, (cv.b, (cv.c, ()))), [cv.d, cv.e, cv.f]), [1, 2, 3])
+        assert match(some((cv.a, (cv.b, (cv.c, ()))), [cv.d, cv.e, cv.f]),
+          [1, 2, 3])
         self.assertEquals(1, cv.d)
         
         try:
@@ -79,12 +93,15 @@ class TestPatmatch(unittest.TestCase):
         except KeyError:
             pass
 
+
     def testPredicate(self):
+        # Test pred() match pattern
         assert match(pred(lambda x: x > 4), 7)
         assert not match(pred(lambda x: x > 4), 2)
 
     
     def testCons(self):
+        # Test cons() match pattern
         cv = CapturedValues()
         assert match(cons(cv.a, cv.b), [1,2,3])
         self.assertEquals(1, cv.a)
@@ -92,19 +109,23 @@ class TestPatmatch(unittest.TestCase):
 
     
     def testEq(self):
+        # Test eq() match pattern
         assert match(eq('abc'), 'abc')
         assert match(eq(int), int)
         assert not match(eq(int), 4)
     
 
     def testType(self):
+        # Test type() match pattern
         assert match(type_is(int), 4)
         assert not match(type_is(int), int)
 
 
     def testObject(self):
+        # Test ob() match pattern
         class Book:
-            def __init__(self, title, author, year=None, publisher=None, location=None):
+            def __init__(self, title, author, year=None, publisher=None,
+              location=None):
                 self.title = title
                 self.author = author
                 self.year = year
@@ -112,8 +133,10 @@ class TestPatmatch(unittest.TestCase):
                 self.location = location
 
         jazz = Book("Tales of the Jazz Age", "F. Scott Fitzgerald", 1922)
-        selfish = Book("The Selfish Gene", "Richard Dawkins", 1976, "Oxford", "London")
-        fifth = Book("Fifth Business", "Robertson Davies", 1970, "Penguin", "Toronto")
+        selfish = Book("The Selfish Gene", "Richard Dawkins",
+            1976, "Oxford", "London")
+        fifth = Book("Fifth Business", "Robertson Davies",
+            1970, "Penguin", "Toronto")
 
         cv = CapturedValues()
         assert match(ob(Book, title=cv.title, author=cv.author), jazz)
@@ -124,23 +147,30 @@ class TestPatmatch(unittest.TestCase):
         assert not match(ob(Book, abc=cv.abc), jazz)
 
         cv = CapturedValues()
-        assert match(ob(Book, title=cv.title, author="Richard Dawkins", year=pred(lambda x: x > 1970)), selfish)
+        assert match(ob(Book, title=cv.title, author="Richard Dawkins",
+          year=pred(lambda x: x > 1970)), selfish)
         self.assertEquals("The Selfish Gene", cv.title)
 
         cv = CapturedValues()
         assert match(ob(title=str, author=str, year=1970), fifth)
 
         cv = CapturedValues()
-        assert not match(ob(Book, title=cv.title, author="Robertson Davies", year=pred(lambda x: x > 1970)), selfish)
+        assert not match(ob(Book, title=cv.title, author="Robertson Davies",
+          year=pred(lambda x: x > 1970)), selfish)
 
 
-    def testHash(self):
+    def testDict(self):
+        # Test dt() match pattern
         cv = CapturedValues()
-        assert match(dt(foo=cv.foo, baz=(1, cv.baz)), {'foo': 6, 'bar': 9, 'baz': (1, 3)})
+        assert match(dt(foo=cv.foo, baz=(1, cv.baz)), 
+          {'foo': 6, 'bar': 9, 'baz': (1, 3)})
         self.assertEquals(6, cv.foo)
         self.assertEquals(3, cv.baz)
 
+
     def testMultipleUses(self):
+        # Test behaviour when a value is captured multiple times in
+        # the match pattern
         cv = CapturedValues()
         assert match((cv.a, cv.b, cv.a), (1, 2, 1))
         self.assertEquals(1, cv.a)        
